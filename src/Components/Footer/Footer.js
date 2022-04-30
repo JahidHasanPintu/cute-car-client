29fca62228790f9e48b7c64a58c4bdae84d51488
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-04-30 12:42:22 +0600

    footer complete

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,10 +4,10 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div>
-            <footer class="footer">
-  	 <div class="container">
-  	 	<div class="row">
-  	 		<div class="footer-col">
+            <footer className="footer">
+  	 <div className="container">
+  	 	<div className="row">
+  	 		<div className="footer-col">
   	 			<h4>company</h4>
   	 			<ul>
   	 				<li><a href="#">about us</a></li>
@@ -16,36 +16,41 @@ const Footer = () => {
   	 				<li><a href="#">affiliate program</a></li>
   	 			</ul>
   	 		</div>
-  	 		<div class="footer-col">
+  	 		<div className="footer-col">
   	 			<h4>get help</h4>
   	 			<ul>
   	 				<li><a href="#">FAQ</a></li>
-  	 				<li><a href="#">shipping</a></li>
-  	 				<li><a href="#">returns</a></li>
-  	 				<li><a href="#">order status</a></li>
-  	 				<li><a href="#">payment options</a></li>
+  	 				<li><a href="#">Support</a></li>
+  	 				<li><a href="#">Contact</a></li>
+  	 				<li><a href="#">Location</a></li>
+  	 				
   	 			</ul>
   	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>online shop</h4>
+  	 		<div className="footer-col">
+  	 			<h4>Our Brands</h4>
   	 			<ul>
-  	 				<li><a href="#">watch</a></li>
-  	 				<li><a href="#">bag</a></li>
-  	 				<li><a href="#">shoes</a></li>
-  	 				<li><a href="#">dress</a></li>
+  	 				<li><a href="#">Mazda</a></li>
+  	 				<li><a href="#">Ferrari</a></li>
+  	 				<li><a href="#">Marcedes</a></li>
+  	 				<li><a href="#">BMW</a></li>
   	 			</ul>
   	 		</div>
-  	 		<div class="footer-col">
+  	 		<div className="footer-col">
   	 			<h4>follow us</h4>
-  	 			<div class="social-links">
-  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
-  	 				<a href="#"><i class="fab fa-twitter"></i></a>
-  	 				<a href="#"><i class="fab fa-instagram"></i></a>
-  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			<div className="social-links">
+  	 				<a href="#"><i className="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i className="fab fa-twitter"></i></a>
+  	 				<a href="#"><i className="fab fa-instagram"></i></a>
+  	 				<a href="#"><i className="fab fa-linkedin-in"></i></a>
   	 			</div>
   	 		</div>
   	 	</div>
+          
   	 </div>
+       <div className="copy-right">
+               
+               <p className='foot-text'>Copyright &copy; Jahid Hasan Pintu | 2022 All right reserved</p>
+           </div>
   </footer>
         </div>
     );
