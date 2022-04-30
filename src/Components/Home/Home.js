bc2d00ffcc231dba3e0285255245024b5e7b014b
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-04-30 12:12:43 +0600

    footer added

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+
+
+const Home = () => {
+    return (
+        <div>
+
+            <Header></Header>
+
+            <h2>This is home</h2>
+        </div>
+    );
+};
+
+export default Home;
