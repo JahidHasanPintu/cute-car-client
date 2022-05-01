6eda55dc4ae2da92d3ff78550aec20cd6c128bfd
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-05-01 20:39:38 +0600

    brand responsiveness

--- src/Components/Brands/Brands.js
+++ src/Components/Brands/Brands.js
@@ -9,85 +9,85 @@ const Brands = () => {
 				 </div>
 					
 					<div className="row justify-content-center align-items-center text-center">
-						<div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-12 brand-colum">
-						<a href="https://listing.maxwheelswp.com/inventory/?category=bmw">
+						<div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-6 brand-colum">
+						<a href="#">
 						<div className="image-bar">
 						<span className="mks-3">
 							<img src="https://listing.maxwheelswp.com/wp-content/uploads/2021/03/bmw.png" alt="BMW"/>
 						</span>
 						</div>
-						<span className="mks-3-name">BMW</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-12 brand-colum">
-						<a href="https://listing.maxwheelswp.com/inventory/?category=mercedes-benz">
+						<span className="mks-3-name">BMW</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-6 brand-colum">
+						<a href="#">
 						<div className="image-bar">
 						<span className="mks-3">
 							<img src="https://listing.maxwheelswp.com/wp-content/uploads/2021/03/mercedes.png" alt="Mercedes Benz"/>
 						</span>
 						</div>
-						<span className="mks-3-name">Mercedes Benz</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-12 brand-colum">
-						<a href="https://listing.maxwheelswp.com/inventory/?category=chevrolet">
+						<span className="mks-3-name">Mercedes Benz</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-6 brand-colum">
+						<a href="#">
 						<div className="image-bar">
 						<span className="mks-3">
 							<img src="https://listing.maxwheelswp.com/wp-content/uploads/2021/03/%D1%81hevrolet_PNG157.png" alt="Chevrolet"/>
 						</span>
 						</div>
-						<span className="mks-3-name">Chevrolet</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-12 brand-colum">
-						<a href="https://listing.maxwheelswp.com/inventory/?category=ferrari">
+						<span className="mks-3-name">Chevrolet</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-6 brand-colum">
+						<a href="#">
 						<div className="image-bar">
 						<span className="mks-3">
 							<img src="https://listing.maxwheelswp.com/wp-content/uploads/2021/04/ferrari_.png" alt="Ferrari"/>
 						</span>
 						</div>
-						<span className="mks-3-name">Ferrari</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-12 brand-colum">
-						<a href="https://listing.maxwheelswp.com/inventory/?category=toyota">
+						<span className="mks-3-name">Ferrari</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-6 brand-colum">
+						<a href="#">
 						<div className="image-bar">
 						<span className="mks-3">
 							<img src="https://listing.maxwheelswp.com/wp-content/uploads/2021/03/toyota-logo.png" alt="Toyota"/>
 						</span>
 						</div>
-						<span className="mks-3-name">Toyota</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-12 brand-colum">
-						<a href="https://listing.maxwheelswp.com/inventory/?category=kia">
+						<span className="mks-3-name">Toyota</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-6 brand-colum">
+						<a href="#">
 						<div className="image-bar">
 						<span className="mks-3">
 							<img src="https://listing.maxwheelswp.com/wp-content/uploads/2021/03/kia_PNG46.png" alt="Kia"/>
 						</span>
 						</div>
-						<span className="mks-3-name">Kia</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-12 brand-colum">
-						<a href="https://listing.maxwheelswp.com/inventory/?category=honda">
+						<span className="mks-3-name">Kia</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-6 brand-colum">
+						<a href="#">
 						<div className="image-bar">
 						<span className="mks-3">
 							<img src="https://listing.maxwheelswp.com/wp-content/uploads/2021/03/honda-logo.png" alt="Honda"/>
 						</span>
 						</div>
-						<span className="mks-3-name">Honda</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-12 brand-colum">
-						<a href="https://listing.maxwheelswp.com/inventory/?category=ford">
+						<span className="mks-3-name">Honda</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-6 brand-colum">
+						<a href="#">
 						<div className="image-bar">
 						<span className="mks-3">
 							<img src="https://listing.maxwheelswp.com/wp-content/uploads/2021/03/ford-logo-icon-png-14228.png" alt="Ford"/>
 						</span>
 						</div>
-						<span className="mks-3-name">Ford</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-12 brand-colum">
-						<a href="https://listing.maxwheelswp.com/inventory/?category=tesla">
+						<span className="mks-3-name">Ford</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-6 brand-colum">
+						<a href="#">
 						<div className="image-bar">
 						<span className="mks-3">
 							<img src="https://listing.maxwheelswp.com/wp-content/uploads/2021/03/tesla_logo_PNG19.png" alt="Tesla"/>
 						</span>
 						</div>
-						<span className="mks-3-name">Tesla</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-12 brand-colum">
-						<a href="https://listing.maxwheelswp.com/inventory/?category=mg">
+						<span className="mks-3-name">Tesla</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-6 brand-colum">
+						<a href="#">
 						<div className="image-bar">
 						<span className="mks-3">
 							<img src="https://listing.maxwheelswp.com/wp-content/uploads/2021/03/car_logo_PNG1657.png" alt="MG"/>
 						</span>
 						</div>
-						<span className="mks-3-name">MG</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-12 brand-colum">
-						<a href="https://listing.maxwheelswp.com/inventory/?category=audi">
+						<span className="mks-3-name">MG</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-6 brand-colum">
+						<a href="#">
 						<div className="image-bar">
 						<span className="mks-3">
 							<img src="https://listing.maxwheelswp.com/wp-content/uploads/2021/03/audi-logo-2.png" alt="Audi"/>
 						</span>
 						</div>
-						<span className="mks-3-name">Audi</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-12 brand-colum">
-						<a href="https://listing.maxwheelswp.com/inventory/?category=alfa-romeo">
+						<span className="mks-3-name">Audi</span></a></div><div className="col-xl-2 col-xxl-2 col-lg-3 col-md-4 col-sm-6 col-6 brand-colum">
+						<a href="#">
 						<div className="image-bar">
 						<span className="mks-3">
 							<img src="https://listing.maxwheelswp.com/wp-content/uploads/2021/04/alfa.png" alt="Alfa Romeo"/>
--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,61 +1,58 @@
 import React from 'react';
 import './Counter.css';
 import CountUp from 'react-countup';
-import { ParallaxProvider } from 'react-scroll-parallax';
-import ParallaxImg from './ParallaxImg/ParallaxImg';
-import img1 from '../../images/car-bg.jpg';
 const Counter = () => {
     return (
         <div>
 			
-        <section className="py-50 bg-img countnm-bx d-flex">
+			<section className="py-50 bg-img countnm-bx d-flex">
 
-			
-		<div className="container">
-			<div className="counter-colection">
-			<h3 className='text-center text-light'>Explore so many Car Quickly And Easily</h3>
-			<p className='text-center text-light'>We believe in making simple and huge number of collection i our car inventory plans. <br /> Choose your next successful plan and get started today!.</p>
-			</div>
-			<div className="row">
-				<div className="col-lg-3 col-md-6 col-12 border-style">
-					<div className="text-center">
-						<div className="w-80 h-80 l-h-100  text-center mx-auto">
-							<span className="text-white fs-40 icon-Selected-file"><span className="path1"></span><span className="path2"></span></span>
-						</div>
-						<h1 className="countnm my-10 text-white fw-300"><CountUp start={0} end={5428} duration={20} /></h1>
-						<div className="text-uppercase text-white">Total Car</div>
-					</div>
-				</div>	
-				<div className="col-lg-3 col-md-6 col-12 border-style">
-					<div className="text-center">
-						<div className="w-80 h-80 l-h-100  text-center mx-auto">
-							<span className="text-white fs-40 icon-Edit"><span className="path1"></span><span className="path2"></span></span>
-						</div>
-						<h1 className="countnm my-10 text-white fw-300"><CountUp start={0} end={5428} duration={20}/></h1>
-						<div className="text-uppercase text-white">New Collection</div>
-					</div>
+				
+			<div className="container">
+				<div className="counter-colection">
+				<h3 className='text-center text-light'>Explore so many Car Quickly And Easily</h3>
+				<p className='text-center text-light'>We believe in making simple and huge number of collection i our car inventory plans. <br /> Choose your next successful plan and get started today!.</p>
 				</div>
-				<div className="col-lg-3 col-md-6 col-12 border-style">
-					<div className="text-center">
-						<div className="w-80 h-80 l-h-100  text-center mx-auto">
-							<span className="text-white fs-40 icon-Briefcase"><span className="path1"></span><span className="path2"></span></span>
+				<div className="row">
+					<div className="col-lg-3 col-md-6 col-12 border-style">
+						<div className="text-center">
+							<div className="w-80 h-80 l-h-100  text-center mx-auto">
+								<span className="text-white fs-40 icon-Selected-file"><span className="path1"></span><span className="path2"></span></span>
+							</div>
+							<h1 className="countnm my-10 text-white fw-300"><CountUp start={0} end={5428} duration={20} /></h1>
+							<div className="text-uppercase text-white">Total Car</div>
 						</div>
-						<h1 className="countnm my-10 text-white fw-300"><CountUp start={0} end={5428} duration={20}/></h1>
-						<div className="text-uppercase text-white">Premium</div>
+					</div>	
+					<div className="col-lg-3 col-md-6 col-12 border-style">
+						<div className="text-center">
+							<div className="w-80 h-80 l-h-100  text-center mx-auto">
+								<span className="text-white fs-40 icon-Edit"><span className="path1"></span><span className="path2"></span></span>
+							</div>
+							<h1 className="countnm my-10 text-white fw-300"><CountUp start={0} end={5428} duration={20}/></h1>
+							<div className="text-uppercase text-white">New Collection</div>
+						</div>
 					</div>
+					<div className="col-lg-3 col-md-6 col-12 border-style">
+						<div className="text-center">
+							<div className="w-80 h-80 l-h-100  text-center mx-auto">
+								<span className="text-white fs-40 icon-Briefcase"><span className="path1"></span><span className="path2"></span></span>
+							</div>
+							<h1 className="countnm my-10 text-white fw-300"><CountUp start={0} end={5428} duration={20}/></h1>
+							<div className="text-uppercase text-white">Premium</div>
+						</div>
+					</div>
+					<div className="col-lg-3 col-md-6 col-12 border-style">
+						<div className="text-center">
+							<div className="w-80 h-80 l-h-100 text-center mx-auto">
+								<span className="text-white fs-40 icon-Add-user"><span className="path1"></span><span className="path2"></span></span>
+							</div>
+							<h1 className="countnm my-10 text-white fw-300"><CountUp start={0} end={5428} duration={20} /></h1>
+							<div className="text-uppercase text-white">Budget CAR</div>
+						</div>
+					</div>			
 				</div>
-				<div className="col-lg-3 col-md-6 col-12 border-style">
-					<div className="text-center">
-						<div className="w-80 h-80 l-h-100 text-center mx-auto">
-							<span className="text-white fs-40 icon-Add-user"><span className="path1"></span><span className="path2"></span></span>
-						</div>
-						<h1 className="countnm my-10 text-white fw-300"><CountUp start={0} end={5428} duration={20} /></h1>
-						<div className="text-uppercase text-white">Budget CAR</div>
-					</div>
-				</div>			
 			</div>
-		</div>
-	</section>
+		</section>
         </div>
     );
 };
