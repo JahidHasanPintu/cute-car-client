1311662af69807adda501bf2febd3985fcbaf184
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-05-04 12:30:39 +0600

    table added in inventory

--- src/Components/ManageInventory/ManageInventory.js
+++ src/Components/ManageInventory/ManageInventory.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
+
 
 const ManageInventory = () => {
     return (
         <div>
-            <h2>Manage inventory</h2>
+             <Table responsive>
+                <thead>
+                    <tr>
+                    <th>#</th>
+                    {Array.from({ length: 12 }).map((_, index) => (
+                        <th key={index}>Table heading</th>
+                    ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>1</td>
+                    {Array.from({ length: 12 }).map((_, index) => (
+                        <td key={index}>Table cell {index}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                    <td>2</td>
+                    {Array.from({ length: 12 }).map((_, index) => (
+                        <td key={index}>Table cell {index}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                    <td>3</td>
+                    {Array.from({ length: 12 }).map((_, index) => (
+                        <td key={index}>Table cell {index}</td>
+                    ))}
+                    </tr>
+                </tbody>
+                </Table>
         </div>
     );
 };
